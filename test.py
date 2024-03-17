7832928c1a393a03763496697abedcc322880ab7
aabeyse1 <93781833+aabeyse1@users.noreply.github.com>
2024-03-16 19:21:18 -0500

    api thing

---
+++ test.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+
+# curl https://api.openai.com/v1/chat/completions \
+#   -H "Content-Type: application/json" \
+#   -H "Authorization: Bearer" \
+#   -d '{
+#      "model": "gpt-3.5-turbo",
+#      "messages": [{"role": "user", "content": "Say this is a test!"}],
+#      "temperature": 0.7
+#    }'
