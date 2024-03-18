4473b466f90371778f841bdcc9ce0a60410d22fa
PassdownInc <116396453+PassdownInc@users.noreply.github.com>
2024-03-17 20:58:17 -0500

    comments added

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ AI and Robotics Final Project (Devinn Chi, Arnika Abeysekera, Quang Nguyen)
 
 helpful links/references we used:
 1. https://www.youtube.com/watch?v=Vurdg6yrPL8&list=PLpdmBGJ6ELUIYHjmzYTuePlNRf7yeCACz
+2. https://platform.openai.com/docs/quickstart?context=python # this is the official openai api startup guide
--- test2.py
+++ test2.py
@@ -1,12 +1,16 @@
+# import statements
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
+# load env reader from import
 load_dotenv()
 
+# load api key from env file and send it to client for the request
 api_key = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=api_key)
 
+# create a couple ai things for testing that are using gpt-3.5-turbo
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
@@ -15,4 +19,5 @@ completion = client.chat.completions.create(
   ]
 )
 
+# print the results
 print(completion.choices[0].message)
